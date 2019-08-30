e3bea2efb10fa8bfb1e69dc0b649055c5d47958b
danie <danie@DESKTOP-D3BM1F7.wireless-1x.unc.edu>
2019-08-30 13:35:18 -0400

    updated with novice done

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,45 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+    
+		int numCustomers= scan.nextInt();
+		for (int i=0; i<numCustomers; i++) {
+			String firstName= scan.next();
+			String lastName= scan.next();
 		
+			int numItems = scan.nextInt();					
+			double accum = 0;
+			for (int j=0; j<numItems; j++) {
+				int[] itemQuantArr = new int[numItems];
+				itemQuantArr[j]= scan.nextInt();
+				
+				String[] itemNameArr = new String[numItems];
+				itemNameArr[j]= scan.next();
+				
+				double[] itemPriceArr = new double[numItems];
+				itemPriceArr[j]= scan.nextDouble();
+				
+				accum+= (itemQuantArr[j]*itemPriceArr[j]);
+				}
+			
+			System.out.println(firstName.charAt(0)+ ". "+ lastName+ ": "+ String.format("%.2f", accum));
+		
+//			scan.close();
+			}
 	}
 }
+
+
+/*
+3
+Carrie Brownstein 3
+2 Banana 0.75
+1 Orange 1.25
+2 Milk 3.15
+Corin Tucker 2
+3 Banana 0.75
+2 Sponge 1.15
+Janet Weiss 1
+5 Salami 2.50
+
+*/
