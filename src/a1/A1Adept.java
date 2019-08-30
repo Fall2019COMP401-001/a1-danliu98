36e3270dd084e733f3b986237d97f4c24f7c5f7a
danie <danie@DESKTOP-D3BM1F7.wireless-1x.unc.edu>
2019-08-30 16:52:02 -0400

    Updated with Adept now

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -5,10 +5,81 @@ import java.util.Scanner;
 public class A1Adept {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int numItems= scan.nextInt();
+		String[] itemName= new String[numItems];
+		double[] itemPrice= new double[numItems];
+
+		for (int i=0; i<numItems; i++) {
+			itemName[i]= scan.next();
+			itemPrice[i]= scan.nextDouble();
+		}
+
+		int numCustomers= scan.nextInt();
+		String[] firstName= new String[numCustomers];
+		String[] lastName= new String[numCustomers];
+		double totalSpent= 0;
+		double[] customerSpent= new double[numCustomers];
 		
+		double largestAmtSpent=0;
+		String largestSpender= "";
+		double smallestAmtSpent= 0;
+		String smallestSpender= "";
+
+		for (int j=0; j<numCustomers; j++) {
+			firstName[j]= scan.next();
+			lastName[j]= scan.next();
+
+			int itemsBought= scan.nextInt();
+			for (int k=0; k<itemsBought; k++) {
+				int[] itemQuant= new int[itemsBought];
+				itemQuant[k]= scan.nextInt();
+
+				String[] boughtItemName= new String[itemsBought];
+				boughtItemName[k]= scan.next();
+
+				for (int m=0; m<numItems; m++) {
+					if (boughtItemName[k].equals(itemName[m])) {customerSpent[j]+= (itemQuant[k]*itemPrice[m]);}
+				}					
+			}
+			totalSpent+= customerSpent[j];
+//			^moved out to only added up 3times (3 customers)
+			
+			largestAmtSpent= customerSpent[0];
+			smallestAmtSpent= customerSpent[0];
+			for (int n=0; n<numCustomers; n++) {
+				if (largestAmtSpent<customerSpent[n]) {
+					largestAmtSpent= customerSpent[n];
+					largestSpender= firstName[n]+ " " +lastName[n]; }
+
+				if (smallestAmtSpent>customerSpent[n]) {
+					smallestAmtSpent= customerSpent[n];
+					smallestSpender= firstName[n]+ " " +lastName[n]; }
+			}
+		}
+		double avgSpent= totalSpent/ numCustomers;
+
+		System.out.println("Biggest: "+ largestSpender + " ("+ largestAmtSpent +")");
+		System.out.println("Smallest: "+ smallestSpender + " ("+ smallestAmtSpent +")");
+		System.out.println("Average: "+ String.format("%.2f", avgSpent));
+
 	}
 }
+
+
+/*
+6
+Apple 0.25
+Banana 0.75
+Milk 3.15
+Orange 1.25
+Salami 2.50
+Sponge 1.15
+3 
+Carrie Brownstein 3 2 Banana 1 Orange 2 Milk
+Corin Tucker 2 3 Banana 2 Sponge
+Janet Weiss 1 5 Salami
+
+*/
